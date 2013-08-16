e7e70850134550afe1368ae447f20981423bc2ad
aabdo <aabdo@salon.com>
2013-08-16 09:15:04 +0000

    initial config

---
+++ config/zodiac-config.php
@@ -0,0 +1,12 @@
+<?php
+
+////////////////////////////////
+// CONFIG
+////////////////////////////////
+
+$config = array(
+                'alphabet' => array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','x')
+             );
+
+//print_r($settings);
+return $config;
