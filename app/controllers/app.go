760512c9476459ff671a789033d26423fe52b314
Ahmad Abdo <aabdo@rampagedesigns.com>
2015-10-03 23:52:57 -0700

    new style updates from Ibu and some other work on the structs

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,16 +11,146 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	cipher := models.NewCipher(revel.Config.StringDefault("cipher.340.raw", ""), revel.Config.IntDefault("cipher.340.cols", 10), revel.Config.BoolDefault("cipher.340.solved", false), revel.Config.StringDefault("cipher.340.translation", ""))
+	cipher := models.NewCipher(
+		revel.Config.StringDefault("cipher.340.raw", ""),
+		revel.Config.IntDefault("cipher.340.cols", 10),
+		revel.Config.BoolDefault("cipher.340.solved", false),
+		map[rune]rune{},
+	)
 	return c.Render(cipher)
 }
 
 func (c App) Z408() revel.Result {
-	cipher := models.NewCipher(revel.Config.StringDefault("cipher.408.raw", ""), revel.Config.IntDefault("cipher.408.cols", 10), revel.Config.BoolDefault("cipher.408.solved", false), revel.Config.StringDefault("cipher.408.translation", ""))
+
+	key := map[rune]rune{
+		'!':  'A',
+		'#':  'A',
+		'$':  'L',
+		'%':  'L',
+		'&':  'Y',
+		'*':  'S',
+		'+':  'E',
+		'/':  'K',
+		'0':  'T',
+		'5':  'E',
+		'6':  'H',
+		'7':  'N',
+		'9':  'D',
+		'@':  'I',
+		'A':  'W',
+		'B':  'L',
+		'D':  'N',
+		'E':  'E',
+		'F':  'S',
+		'G':  'A',
+		'H':  'T',
+		'J':  'F',
+		'K':  'S',
+		'L':  'T',
+		'M':  'H',
+		'N':  'E',
+		'O':  'N',
+		'P':  'I',
+		'Q':  'F',
+		'R':  'G',
+		'S':  'A',
+		'T':  'O',
+		'U':  'I',
+		'V':  'B',
+		'W':  'E',
+		'X':  'O',
+		'Y':  'U',
+		'Z':  'E',
+		'[':  'T',
+		'\\': 'R',
+		']':  'O',
+		'c':  'V',
+		'd':  'O',
+		'e':  'C',
+		'f':  'D',
+		'j':  'X',
+		'k':  'I',
+		'l':  'A',
+		'p':  'E',
+		'q':  'M',
+		'r':  'R',
+		't':  'R',
+		'v':  'N',
+		'~':  'P',
+	}
+
+	cipher := models.NewCipher(
+		revel.Config.StringDefault("cipher.408.raw", ""),
+		revel.Config.IntDefault("cipher.408.cols", 10),
+		revel.Config.BoolDefault("cipher.408.solved", false),
+		key,
+	)
 	return c.Render(cipher)
 }
 
 func (c App) Z340() revel.Result {
-	cipher := models.NewCipher(revel.Config.StringDefault("cipher.340.raw", ""), revel.Config.IntDefault("cipher.340.cols", 10), revel.Config.BoolDefault("cipher.340.solved", false), revel.Config.StringDefault("cipher.340.translation", ""))
+
+	key := map[rune]rune{
+		'!':  'A',
+		'#':  'A',
+		'$':  'L',
+		'%':  'L',
+		'&':  'Y',
+		'*':  'S',
+		'+':  'E',
+		'/':  'K',
+		'0':  'T',
+		'5':  'E',
+		'6':  'H',
+		'7':  'N',
+		'9':  'D',
+		'@':  'I',
+		'A':  'W',
+		'B':  'L',
+		'D':  'N',
+		'E':  'E',
+		'F':  'S',
+		'G':  'A',
+		'H':  'T',
+		'J':  'F',
+		'K':  'S',
+		'L':  'T',
+		'M':  'H',
+		'N':  'E',
+		'O':  'N',
+		'P':  'I',
+		'Q':  'F',
+		'R':  'G',
+		'S':  'A',
+		'T':  'O',
+		'U':  'I',
+		'V':  'B',
+		'W':  'E',
+		'X':  'O',
+		'Y':  'U',
+		'Z':  'E',
+		'[':  'T',
+		'\\': 'R',
+		']':  'O',
+		'c':  'V',
+		'd':  'O',
+		'e':  'C',
+		'f':  'D',
+		'j':  'X',
+		'k':  'I',
+		'l':  'A',
+		'p':  'E',
+		'q':  'M',
+		'r':  'R',
+		't':  'R',
+		'v':  'N',
+		'~':  'P',
+	}
+	cipher := models.NewCipher(
+		revel.Config.StringDefault("cipher.340.raw", ""),
+		revel.Config.IntDefault("cipher.340.cols", 10),
+		revel.Config.BoolDefault("cipher.340.solved", false),
+		key,
+	)
 	return c.Render(cipher)
 }
--- app/models/ciphers.go
+++ app/models/ciphers.go
@@ -12,7 +12,8 @@ type Cipher struct {
 	Symbols     []rune // The actual cipher
 	Translation string
 	Solved      bool
-	Key         map[string][]string
+	Key         map[rune]rune
+	LettersKey  map[string][]string
 	SymbolsKey  map[string][]string
 }
 
@@ -22,15 +23,29 @@ type Character struct {
 	EndOfWord bool
 }
 
-func NewCipher(str string, cols int, solved bool, translation string) *Cipher {
+func NewCipher(str string, cols int, solved bool, key map[rune]rune) *Cipher {
 	c := new(Cipher)
 	c.Symbols = []rune(str)
 	c.Length = len(str)
+	c.Key = key
+	c.doTranslation()
+
 	c.Cols = cols
 	c.Rows = c.Length / c.Cols
-	c.Solved = solved
-	c.Translation = translation
+	c.Solved = solved // boolean
+
 	return c
+}
+
+func (c *Cipher) doTranslation() {
+
+	temp := make([]rune, c.Length)
+
+	for s, symbol := range c.Symbols {
+		temp[s] = c.Key[symbol]
+	}
+
+	c.Translation = string(temp)
 }
 
 func (c *Cipher) GetSymbol(index int) string {
@@ -61,54 +76,35 @@ func (c *Cipher) DisplayCipher() [][]Character {
 	return d
 }
 
-func (c *Cipher) DisplayKey() map[string][]string {
-	c.BuildKey()
+// Display Letters Key
+func (c *Cipher) DisplayKey() map[string]string {
 
-	return c.Key
+	stringMap := make(map[string]string, c.Length)
+
+	for symbol, letter := range c.Key {
+		stringMap[string(symbol)] = string(letter)
+	}
+
+	return stringMap
 }
 
-func (c *Cipher) DisplaySymbols() map[string][]string {
-	c.BuildSymbols()
-
-	return c.SymbolsKey
-}
-
-func (c *Cipher) BuildKey() {
+// Build Symbols Key
+func (c *Cipher) BuildLettersKey() {
 
 	t := []byte(strings.Replace(c.Translation, " ", "", -1))
 
 	if c.Key == nil {
-		c.Key = make(map[string][]string)
+		c.LettersKey = make(map[string][]string)
 	}
 
 Loop:
 	for i, letter := range t {
 		nS := string(c.Symbols[i])
-		for _, existing := range c.Key[string(letter)] {
+		for _, existing := range c.LettersKey[string(letter)] {
 			if existing == nS {
 				continue Loop
 			}
 		}
-		c.Key[string(letter)] = append(c.Key[string(letter)], nS)
-	}
-}
-
-func (c *Cipher) BuildSymbols() {
-
-	t := []byte(strings.Replace(c.Translation, " ", "", -1))
-
-	if c.SymbolsKey == nil {
-		c.SymbolsKey = make(map[string][]string)
-	}
-
-Loop:
-	for i, letter := range t {
-		nS := string(c.Symbols[i])
-		for _, existing := range c.Key[string(letter)] {
-			if existing == string(letter) {
-				continue Loop
-			}
-		}
-		c.SymbolsKey[nS] = append(c.SymbolsKey[nS], string(letter))
+		c.LettersKey[string(letter)] = append(c.LettersKey[string(letter)], nS)
 	}
 }
